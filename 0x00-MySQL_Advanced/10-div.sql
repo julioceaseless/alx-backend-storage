4b05ec39f609dc0aa30097073562718796de47e8
julioceaseless <julioceaseless@gmail.com>
2024-07-18 20:15:36 +0000

    fix bug

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,8 +1,8 @@
---- creates a function SafeDiv that divides (and returns) the first
+-- creates a function SafeDiv that divides (and returns) the first
 -- by the second number or returns 0 if the second number is equal to 0.
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER //
-CREATE FUNCTION SafeDiv(a INT, b INT)
+CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
     DECLARE result FLOAT DEFAULT 0;
