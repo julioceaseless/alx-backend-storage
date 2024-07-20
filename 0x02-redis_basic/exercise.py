8b944d55aa55aa4ec9bd59445dfba8db026c070d
julioceaseless <julioceaseless@gmail.com>
2024-07-20 02:12:17 +0000

    Add wrapper function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,19 @@ Redis basic
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    decorator
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -20,6 +33,7 @@ class Cache():
         # clear Redis database
         self._redis.flushdb
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]):
         """
         Store data in Redis using a randomly generated key
